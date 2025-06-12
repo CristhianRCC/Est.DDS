5dd144cd7c6f626696c4c655003d8463150a9e1a
GRUPO 01 <cristhianrcampos2@gmail.com>
2025-06-12 18:23:32 -0300

    adding stack.h complete

---
+++ stack.h
@@ -0,0 +1,15 @@
+#ifndef stack_h
+#define stack_h
+
+typedef struct stack Stack;
+
+Stack* new_stack(int size);
+void stack_push(Stack* s, int value);
+int stack_pop (Stack* s);
+void stack_print(Stack* s);
+void add(Stack* s);
+void sub(Stack* s);
+void div(Stack* s);
+
+
+#endif
