b93a46a8f09519368034f6cf9ac99f38251adee6
CristhianRCC <cristhianrcampos2@gmail.com>
2025-06-23 23:14:57 -0300

    Update interpret.c

--- interpret.c
+++ interpret.c
@@ -1,33 +1,94 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "interpret.h"
 #include "stack.h"
-Stack *s = NULL;
+#include "lista.h"
 
-void interpreter_init(){
-    s = new_stack(100);
-}
+extern struct list mem_vars;
 
-void interpret (const char *source) {
+void interpretar_comando(const char *source) {
     char op[10];
-    char arg[10];
+    char arg[20];
+    int x, y;
 
-    sscanf (source, "%s%s", op, arg);
-    printf("operaÃ§Ã£o: %s\n", op);
-    printf("argumento: %s\n",  arg);
-    if(strcmp(op, "push")==0){
-        int value = atoi(arg);
-        stack_push(s, value);
+    sscanf(source, "%s%s", op, arg);
+
+    if (strcmp(op, "push") == 0) {
+        int valor;
+        if (sscanf(arg, "%d", &valor) == 1) {
+            push(valor);
+        } else {
+            if (buscar_valor(&mem_vars, arg, &valor)) {
+                push(valor);
+            } else {
+                printf("Variável '%s' não encontrada.\n", arg);
+            }
+        }
     }
-    else if(strcmp(op, "add")==0){
-        add(s);
-    }else if(strcmp(op, "sub")==0){
-        sub(s);
-    }else if(strcmp(op, "div")==0){
-        div(s);
-    }else if(strcmp(op, "print")==0){
-        stack_print(s);
+
+    else if (strcmp(op, "pop") == 0) {
+        if (is_empty()) {
+            printf("Erro: pilha vazia.\n");
+            return;
+        }
+        int valor = pop();
+        adicionar_ou_atualizar(&mem_vars, arg, valor);
+    }
+
+    else if (strcmp(op, "add") == 0) {
+        if (size() < 2) {
+            printf("Erro: poucos elementos na pilha.\n");
+            return;
+        }
+        x = pop();
+        y = pop();
+        push(x + y);
+    }
+
+    else if (strcmp(op, "sub") == 0) {
+        if (size() < 2) {
+            printf("Erro: poucos elementos na pilha.\n");
+            return;
+        }
+        x = pop();
+        y = pop();
+        push(x - y);
+    }
+
+    else if (strcmp(op, "mul") == 0) {
+        if (size() < 2) {
+            printf("Erro: poucos elementos na pilha.\n");
+            return;
+        }
+        x = pop();
+        y = pop();
+        push(x * y);
+    }
+
+    else if (strcmp(op, "div") == 0) {
+        if (size() < 2) {
+            printf("Erro: poucos elementos na pilha.\n");
+            return;
+        }
+        x = pop();
+        y = pop();
+        if (y == 0) {
+            printf("Erro: divisão por zero.\n");
+            return;
+        }
+        push(x / y);
+    }
+
+    else if (strcmp(op, "print") == 0) {
+        if (is_empty()) {
+            printf("Erro: pilha vazia.\n");
+        } else {
+            printf("%d\n", pop());
+        }
+    }
+
+    else {
+        printf("Comando '%s' não reconhecido.\n", op);
     }
 }
